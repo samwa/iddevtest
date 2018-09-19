d630310c2c4393a98fac3eaadfd2a10d8eb492e0
samwaoz <samwa_oz@yahoo.co.uk>
2018-09-19 15:22:03 +1000

    add app service plan to terraform script

--- iddevtest.tf
+++ iddevtest.tf
@@ -6,3 +6,14 @@ resource "azurerm_resource_group" "test" {
   name     = "rg-iddevtest"
   location = "Australia South East"
 }
+
+resource "azurerm_app_service_plan" "test" {
+  name                = "asp-iddevtest"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
