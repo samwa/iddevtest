f02e06dbab885199ec49d3a70047888d0e3a3c96
samwaoz <samwa_oz@yahoo.co.uk>
2018-09-19 16:26:01 +1000

    adding app service to terraform script

--- iddevtest.tf
+++ iddevtest.tf
@@ -2,6 +2,14 @@ provider "azurerm" "test" {
   
 }
 
+resource "random_id" "server" {
+  keepers = {
+    azi_id = 1
+  }
+
+  byte_length = 8
+}
+
 resource "azurerm_resource_group" "test" {
   name     = "rg-iddevtest"
   location = "Australia South East"
@@ -13,7 +21,19 @@ resource "azurerm_app_service_plan" "test" {
   resource_group_name = "${azurerm_resource_group.test.name}"
 
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "test" {
+  name                = "app-${random_id.server.hex}"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.test.id}"
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
   }
 }
