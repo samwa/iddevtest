0dd03599487c045c23bd0ecddfcde8e485e62dca
samwaoz <samwa_oz@yahoo.co.uk>
2018-09-22 11:40:50 +1000

    call webapi from angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private _httpService: Http) { }
+
   title = 'id-dev-test';
+  apiValues: string[] = [];
+
+  ngOnInit() {
+    this._httpService.get('/api/values').subscribe(values => {
+        this.apiValues = values.json() as string[];
+    });
+   }
 }
