4eb74d0a302ee4222cbfe873838b40e30f2a2046
samwaoz <samwa_oz@yahoo.co.uk>
2018-09-24 22:07:21 +1000

    making tests for controller pass

--- iddevtest.tests/LgaController_Tests.cs
+++ iddevtest.tests/LgaController_Tests.cs
@@ -12,20 +12,34 @@ namespace iddevtest.Tests
         {
             
             var options = new DbContextOptionsBuilder<LgaContext>()
-                .UseInMemoryDatabase(databaseName: "Find_searches_url")
+                .UseInMemoryDatabase(databaseName: "Controller_Returns_Results")
                 .Options;
             using (var context = new LgaContext(options))
             {
-                var LgaController = new LgaController(context);
+                
+                context.Lgas.Add(new LgaModel { Place = "Albury (C)", State = "New South Wales", SEIFADisadvantage2011 = 979, SEIFADisadvantage2016 = 971 });
+                context.Lgas.Add(new LgaModel { Place = "Alpine (S)", State = "Victoria", SEIFADisadvantage2011 = 987, SEIFADisadvantage2016 = 994 });
+                context.Lgas.Add(new LgaModel { Place = "Aurukun (S)", State = "Queensland", SEIFADisadvantage2011 = 483, SEIFADisadvantage2016 = 504 });
+                context.SaveChanges();
+
+                LgaController = new LgaController(context);
             }
         }
 
         [Test]
-        public void LgaController_IsIndex()
+        public void LgaController_HasResultsForNSW()
         {
             var actionResult = LgaController.Get("nsw");
-            Assert.That(actionResult.Result, Has.Count.GreaterThan(0));
-            //Assert.Fail();
+            Assert.That(actionResult.Value, Is.Not.Null);
+            Assert.That(actionResult.Value, Has.Count.GreaterThan(0));
+        }
+
+        [Test]
+        public void LgaController_HasNoResultsForSA()
+        {
+            var actionResult = LgaController.Get("sa");
+            Assert.That(actionResult.Value, Is.Not.Null);
+            Assert.That(actionResult.Value, Has.Count.EqualTo(0));
         }
     }
 }
